0da51a35a4ff37c8369a66f6e9aae49086150dab
iawodeji <iawodeji@my.nl.edu>
2024-07-23 21:02:41 -0400

    Update grading.py
    Updated with comments

--- grading.py
+++ grading.py
@@ -19,19 +19,25 @@
 # Grade: F
 # Student is failing.
 
-
+# Prompt user to input three exam grades and convert them to integers
 exam_one = int(input("Input exam grade one: "))
 exam_two = int(input("Input exam grade two: "))
 exam_three = int(input("Input exam grade three: "))
 
+# Store grades in a list
 grades = [exam_one, exam_two, exam_three]
+
+# Initialize the sum variable to 0
 sum = 0
+
+# Loop through the grades list to calculate the total sum of grades
 for grade in grades: 
     sum += grade
 
+# Calculate the average grade
 avg = sum / len(grades)
 
-
+# Determine the letter grade based on the average
 if avg >= 90:
     letter_grade = "A"
 elif avg >= 80:
@@ -43,13 +49,17 @@ elif avg >= 60:
 else:
     letter_grade = "F"
 
-
+# Print each exam grade
 for grade in grades:
     print("Exam: " + str(grade))
 
+# Print the average grade
 print("Average: " + str(avg))
+
+# Print the corresponding letter grade
 print("Grade: " + letter_grade)
 
+# Print whether the student is passing or failing based on the letter grade
 if letter_grade == "F":
     print("Student is failing.")
 else:
