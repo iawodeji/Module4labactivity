0dca68f1b0647842f9a82a93b51eafd08d238592
iawodeji <iawodeji@my.nl.edu>
2024-07-23 21:05:24 -0400

    Update time.py
    Updated with comments

--- time.py
+++ time.py
@@ -1,11 +1,18 @@
 
+# Prompt the user to input the current time in hours (0-23) and store it as a string
 currentTimeStr = input("What is the current time (in hours 0-23)?")
+
+# Prompt the user to input the number of hours to wait and store it as a string
 waitTimeStr = input("How many hours do you want to wait?")
 
+# Convert the current time from a string to an integer
 currentTimeInt = int(currentTimeStr)
+
+# Convert the wait time from a string to an integer
 waitTimeInt = int(waitTimeStr)
 
 
 finalTimeInt = (currentTimeInt + waitTimeInt) % 24
 
+# Print the final time
 print(finalTimeInt)
