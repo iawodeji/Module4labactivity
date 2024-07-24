c4fd7f3450fa05278d2a55078f6ca9fe84f885ea
iawodeji <iawodeji@my.nl.edu>
2024-07-23 21:03:54 -0400

    Update pirate.py
    Updated with comments

--- pirate.py
+++ pirate.py
@@ -1,5 +1,8 @@
 
+# Prompt the user to input a password and store it in the variable 'greeting'
 greeting = input("Hello, possible pirate! What's the password? ")
+
+# Check if the input password is "Arrr!"
 if greeting == "Arrr!":
     print("Go away, pirate.")
 else:
